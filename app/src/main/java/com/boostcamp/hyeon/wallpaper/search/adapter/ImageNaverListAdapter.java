9b32012750e1a192febab3fd15e06c75d808de77
Jun Hyeon KIM <junhyeon47@gmail.com>
2017-02-22 17:08:48 +0900

    Class name modification.

--- app/src/main/java/com/boostcamp/hyeon/wallpaper/search/adapter/ImageNaverListAdapter.java
+++ app/src/main/java/com/boostcamp/hyeon/wallpaper/search/adapter/ImageNaverListAdapter.java
@@ -11,7 +11,7 @@ import com.boostcamp.hyeon.wallpaper.R;
 import com.boostcamp.hyeon.wallpaper.base.adapter.RealmListAdapterContract;
 import com.boostcamp.hyeon.wallpaper.base.domain.ImageNaver;
 import com.boostcamp.hyeon.wallpaper.base.listener.OnItemClickListener;
-import com.boostcamp.hyeon.wallpaper.search.adapter.holder.ImageNaverViewHolder;
+import com.boostcamp.hyeon.wallpaper.search.adapter.holder.ImageNaverListViewHolder;
 
 import io.realm.OrderedRealmCollection;
 import io.realm.RealmRecyclerViewAdapter;
@@ -20,7 +20,7 @@ import io.realm.RealmRecyclerViewAdapter;
  * Created by hyeon on 2017. 2. 21..
  */
 
-public class ImageNaverListAdapter extends RealmRecyclerViewAdapter<ImageNaver, ImageNaverViewHolder> implements RealmListAdapterContract.Model<ImageNaver>, RealmListAdapterContract.View{
+public class ImageNaverListAdapter extends RealmRecyclerViewAdapter<ImageNaver, ImageNaverListViewHolder> implements RealmListAdapterContract.Model<ImageNaver>, RealmListAdapterContract.View{
     private OnItemClickListener mOnItemClickListener;
 
     public ImageNaverListAdapter(@NonNull Context context, @Nullable OrderedRealmCollection<ImageNaver> data, boolean autoUpdate) {
@@ -28,17 +28,17 @@ public class ImageNaverListAdapter extends RealmRecyclerViewAdapter<ImageNaver, 
     }
 
     @Override
-    public ImageNaverViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ImageNaverListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         Context context = parent.getContext();
         int layoutIdForItem = R.layout.item_image;
         LayoutInflater inflater = LayoutInflater.from(context);
 
         View view = inflater.inflate(layoutIdForItem, parent, false);
-        return new ImageNaverViewHolder(view, context, mOnItemClickListener);
+        return new ImageNaverListViewHolder(view, context, mOnItemClickListener);
     }
 
     @Override
-    public void onBindViewHolder(ImageNaverViewHolder holder, int position) {
+    public void onBindViewHolder(ImageNaverListViewHolder holder, int position) {
         holder.bind(getData().get(position), position);
     }
 
